2083482a765b383ad817f16adebdac638df68481
vettrivikas <32515723+vettrivikas@users.noreply.github.com>
2024-10-07 17:15:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, World! This is a DEV Setup Flask app."
-
-if __name__ == '__main__':
-    app.run(debug=True)
+print("Hello Pulled Data")
